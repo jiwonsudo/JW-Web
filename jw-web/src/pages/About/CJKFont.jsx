3d89fbe962ff4f9caba3d19fa8f510a0653abe99
Jiwon | 지원 <79065639+jiwonsudo@users.noreply.github.com>
2025-06-06 05:03:53 +0000

    Style: Noto Serif CJK-KR 폰트 서브셋 업로드 & Base64 Encoded font-face jsx 임시 저장

---
+++ jw-web/src/pages/About/CJKFont.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const FontFace = styled.div`
+  @font-face {
+    font-family: 'Noto Serif CJK KR Bold';
+    src:   url('data:font/woff2;charset=utf-8;base64,d09GMk9UVE8AAAKkAAkAAAAABOQAAAJfAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAADYIbBmAALAE2AiQDBgQGBYQUByAbQAQALgpjY9bYQ9R6UpOI8z2cnXxL1Qye//wj75uZ9ayAUnTYkh1YCrW6FJVc52wb/h8vzG1HSl9E73hy7xex0JCUqPQIWng5AJrgROybiLziNXs7cE5elGAFDXli2RglNAyCh+D5OP8b7KeBPd7GqFAhCMzQ0ud5ovUKRiEiYZ1bY++qhIY63WytsF271ZaBEClyY924kzbBjRc3QbmJ2o0yv+X3lV/Pf4V5xPletinOYqCzYtOdizbNWQ+mKuIZ0PPzQ2/WqaaRN+2PXDcwd9gm2xVh1vdBexz0F0e/vVl2QYw+govQY1svwrzmcgB8DwEoEZUV8OT3jnAn4PF9x5z8dPLpnKGT/IWr85/pk81Pdt51aGLLcTjjHYCI+LgJNuOMcz3klA3+tSvMPRZOWX9U4QHqhBoGtMkfl0nsygClvKhEkEHTOy4Bs/mNeltg9i4BQACUUu9AEgAEjQAWQxLAYBD4b/8nyb8TgFMF7j26e2kqsPy7t9abgJc/XiSUCzt9n02l3oegUQAImL03zHYLvBv7EFCeFb0pKAPdQAFejCojxgc4BmNWSPFkFd7cZjXNfqwhz5/1YMsy1uZJxs5Qx+MrhAwaXQx9ScKiMXrQmMwjUqCrQiSmcETQmkviCfmSL+hdGf3aeoOYb4jCoawhCc0nZ7uO3q2PgUMR8/z4SkRKFZra2C5vXUx4bDIGiebkyNKxWRqtYaOJvCXU/teRsZmjibnXGMl/WB2OGDwujEN8coUYSPel5qdUdJH98Q7OiENgUdKSimQziFikDgGdTQzQ15OXYWr/DyoBAA==') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
+`;
