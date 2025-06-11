a194856b6142a4cb7545580da8cfa9eff6374e35
Jiwon | 지원 <79065639+jiwonsudo@users.noreply.github.com>
2025-06-11 14:56:18 +0000

    Design: InfoPreview Link 완성

--- jw-web/src/pages/Home/ui/InfoPreview.jsx
+++ jw-web/src/pages/Home/ui/InfoPreview.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+
 import { moveUpToAppear } from "./moveUpToAppear";
 import { media } from "../../../shared/styles/mediaQuery";
 
@@ -21,7 +24,7 @@ const InfoCardWrapper = styled.div`
   `}
 `;
 
-const InfoCard = styled.div`
+const InfoCard = styled(Link)`
   width: calc((100% - (${INFOCARD_GAP}px * 2)) / 3);
   aspect-ratio: 1 / 1.6;
   background: url(${(props) => props.$url}) center;
@@ -30,6 +33,7 @@ const InfoCard = styled.div`
   opacity: 0;
   animation: ${moveUpToAppear} .5s cubic-bezier(.46,-0.04,.48,1) forwards;
   animation-delay: ${(props) => props.$delay || '0'};
+  position: relative;
 
   ${media.mobile`
     width: 100%;
@@ -40,14 +44,56 @@ const InfoCard = styled.div`
   `}
 `;
 
+const InfoCardOverLay = styled.div`
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1));
+  border-radius: 20px;
+  position: absolute;
+  top: 0; left: 0;
+  z-index: 2;
+`;
+
+const InfoCardTextBox = styled.div`
+  position: relative;
+  margin: 1rem;
+  z-index: 3;
+`
+
+const InfoCardTitle = styled.h1`
+  color: #FAF9F6;
+  font-size: x-large;
+  font-weight: 600;
+  line-height: 1.2em;
+`;
 
 
 export const InfoPreview = () => {
+  const { t } = useTranslation();
+
   return (
     <InfoCardWrapper>
-      <InfoCard $delay='.2s' $url='/images/info-preview/projects.webp'/>
-      <InfoCard $delay='.3s' $url='/images/info-preview/skill_set.webp'/>
-      <InfoCard $delay='.4s' $url='/images/info-preview/core_values.webp'/>
+      <InfoCard to='/showcase' $delay='.2s' $url='/images/info-preview/projects.webp'>
+        <InfoCardOverLay/>
+        <InfoCardTextBox>
+          {/* Todo: 임시 텍스트 */}
+          <InfoCardTitle>손길을 거친<br/>프로젝트들</InfoCardTitle>
+        </InfoCardTextBox>
+      </InfoCard>
+      <InfoCard to='/about' $delay='.3s' $url='/images/info-preview/skill_set.webp'>
+        <InfoCardOverLay/>
+        <InfoCardTextBox>
+          {/* Todo: 임시 텍스트 */}
+          <InfoCardTitle>꾸준히 발전시켜 온<br/>기술들</InfoCardTitle>
+        </InfoCardTextBox>
+      </InfoCard>
+      <InfoCard to='/about' $delay='.4s' $url='/images/info-preview/core_values.webp'>
+        <InfoCardOverLay/>
+        <InfoCardTextBox>
+          {/* Todo: 임시 텍스트 */}
+          <InfoCardTitle>결정의 바탕이 되는<br/>기준들</InfoCardTitle>
+        </InfoCardTextBox>
+      </InfoCard>
     </InfoCardWrapper>
   );
 }
